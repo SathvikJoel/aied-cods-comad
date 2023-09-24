2b89369d75eabb92d319f11631ac5cf8c4e4d20a
Sathvik Joel K <ksjoe30@gmail.com>
2023-09-24 15:33:47 +0900

    added hugging face softmax loss loop

--- src/training_hf_softmaxLoss.py
+++ src/training_hf_softmaxLoss.py
@@ -11,6 +11,9 @@ def get_transcript(video_name):
     else:
         return val 
     
+# set device
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
 # read the train and dev datasets
 data = pd.read_csv(os.path.join('..', 'input', 'train_folds.csv'))
 train_df = data[data['kfold'] != 1][['pre requisite', 'concept', 'label']]
@@ -69,6 +72,11 @@ def mean_pool(token_embeds, attention_mask):
     )
     return pool
 
+ffnn = torch.nn.Linear(768 * 3, 1)
+ffnn.to(device)
+
+loss_func = torch.nn.CrossEntropyLoss()
+
 from transformers.optimization import get_linear_schedule_with_warmup
 
 optim = torch.optim.Adam(model.parameters(), lr=2e-5)
@@ -85,8 +93,62 @@ num_epoch = 1
 for epoch in range(num_epoch):
     #train
     model.train()
-    
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids_a = batch('pre_requisite_input_ids').to(device)
+        input_ids_b = batch('concept_input_ids').to(device)
+        attention_a = batch('pre_requisite_attention_mask').to(device)
+        attention_b = batch('concept_attention_mask').to(device)
+        label = batch('label').to(device)
+        embed_a = model(input_ids_a, attention_a)[0]
+        embed_b = model(input_ids_b, attention_b)[0]
+        u = mean_pool(embed_a, attention_a)
+        v = mean_pool(embed_b, attention_b)
 
-
+        # build the |u-v| tensor
+        uv = torch.abs(torch.abs(u - v))
+        # concatenate the [u, v, |u-v|] tensors
+        x = torch.cat([u, v, uv], -1)
+        # pass through the final linear layer
+        x = ffnn(x)
+        # compute the loss
+        loss = loss_func(x, label)
+        loss.backward()
+        optim.step()
+        scheduler.step()
+        print(f'epoch : {epoch}, loss : {loss.item():.4f}')
+        # make predictions
+        preds = torch.argmax(x, dim=1)
+        # compute the accuracy
+        acc = (preds == label).float().mean()
+        print(f'epoch : {epoch}, acc : {acc.item():.4f}')
     #eval
     model.eval()
+    for batch in dev_loader:
+        input_ids_a = batch('pre_requisite_input_ids').to(device)
+        input_ids_b = batch('concept_input_ids').to(device)
+        attention_a = batch('pre_requisite_attention_mask').to(device)
+        attention_b = batch('concept_attention_mask').to(device)
+        label = batch('label').to(device)
+        embed_a = model(input_ids_a, attention_a)[0]
+        embed_b = model(input_ids_b, attention_b)[0]
+        u = mean_pool(embed_a, attention_a)
+        v = mean_pool(embed_b, attention_b)
+
+        # build the |u-v| tensor
+        uv = torch.abs(torch.abs(u - v))
+        # concatenate the [u, v, |u-v|] tensors
+        x = torch.cat([u, v, uv], -1)
+        # pass through the final linear layer
+        x = ffnn(x)
+        # compute the loss
+        loss = loss_func(x, label)
+        print(f'epoch : {epoch}, loss : {loss.item():.4f}')
+        # make predictions
+        preds = torch.argmax(x, dim=1)
+        # compute the accuracy
+        acc = (preds == label).float().mean()
+        print(f'epoch : {epoch}, acc : {acc.item():.4f}')
+
+# save the model usig save_pretrained
+model.save_pretrained('bert_ffnn')
